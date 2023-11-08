133cf51a644a6737494b7dd4fa6a8afd26d019e6
arcillamarianec <marianechristene.arcilla.cics@ust.edu.ph>
2023-11-08 13:58:59 +0800

    Initial commit

---
+++ ArcillaITELEC3C/app/Http/Controllers/CategoryController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Category;
+
+class CategoryController extends Controller
+{
+    public function store(Request $request)
+    {
+        $category = new Category();
+        $category->name = $request->input('name');
+        $category->save();
+    
+        return redirect()->route('categories.index');
+    }
+    public function index()
+{
+    $categories = Category::all();
+    return view('categories.index', ['categories' => $categories]);
+}
+}
